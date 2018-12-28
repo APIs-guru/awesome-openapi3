76f6909372dd9b06d3fcb14ca317883dc56449af
Mike Ralphson <mike.ralphson@gmail.com>
2018-12-28 01:52:40 +0000

    Restore rest of 0b31340

--- add.js
+++ add.js
@@ -1,31 +1,56 @@
 'use strict';
 
 const fs = require('fs');
+
 const yaml = require('js-yaml');
+const graphql = require('graphql-request').GraphQLClient;
 
 const estr = fs.readFileSync('./docs/_data/tools.yaml','utf8');
 const entries = yaml.load(estr);
 
-const str = fs.readFileSync('./response.json','utf8');
-const res = JSON.parse(str);
-
-let found = 0;
-
-for (let edge of res.data.search.edges) {
-    let url = edge.node.url;
-    let entry = entries.find(function(e,i,a){
-        return e.github && e.github.toLowerCase() === url.toLowerCase();
-    });
-    if (entry) {
-        found++;
+const query = `{
+	search(type: REPOSITORY, query: "topic:openapi3", first: 100) {
+    edges{
+      node {
+        ... on Repository {
+          url
+        }
+      }
     }
-    else {
-        entries.push({ github: url, v3: true, category: 'unclassified' });
-        console.log('New',url);
+  }
+}`
+
+let existing = 0;
+let added = 0;
+
+const client = new graphql('https://api.github.com/graphql', { headers: {
+    Authorization: 'Basic '+Buffer.from(process.env.github_user+':'+process.env.github_pwd).toString('base64')
+}});
+
+client.request(query).then(function(data){
+
+    for (let edge of data.search.edges) {
+        let url = edge.node.url;
+        let entry = entries.find(function(e,i,a){
+            return e.github && e.github.toLowerCase() === url.toLowerCase();
+        });
+        if (entry) {
+            existing++;
+        }
+        else {
+            entries.push({ github: url, v3: true, category: 'unclassified' });
+            console.log('New',url);
+            added++;
+        }
     }
-}
+})
+.catch(function(ex){
+    console.error(ex.message);
+});
 
 process.on('exit',function(){
-    console.log('Found',found);
-    fs.writeFileSync('./docs/_data/tools.yaml',yaml.dump(entries),'utf8');
+    console.log('Existing:',existing,'added:',added);
+    if (added) {
+        fs.writeFileSync('./docs/_data/tools.yaml',yaml.dump(entries),'utf8');
+    }
 });
